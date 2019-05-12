5bd0d48f0654ab694cb578b0d3a18cb55edc9085
thijs <t.lugtenberg@gmail.com>
2019-05-12 16:45:58 +0200

    brain init do-over

---
+++ brian.py
@@ -0,0 +1,16 @@
+import numpy as np
+import random
+from pygame.math import Vector2
+
+class brian:
+        
+
+    def __init__(self):
+        directions = [ [] in range (size)]
+
+    def Randolm(self, size):
+        for x in self.size:
+          position = pg.math.Vector2(100, 200)
+          velocity = pg.math.Vector2(8, 0).rotate(random.randrange(360))
+          position += velocity
+          print position 
